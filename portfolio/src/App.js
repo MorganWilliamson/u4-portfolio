b2d202cf5546023b0d808d7681082ad7bfb8d11a
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-13 11:21:32 -0600

    Setting up project, clearing out unnecessary files.

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Application is rendering.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
