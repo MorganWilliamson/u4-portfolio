12c9a75640596d0d434d876f76c332ea3a7350f5
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-28 19:05:02 -0600

    Setting up linter, starting work on Navbar.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <p>Application is rendering.</p>
       </header>
     </div>
