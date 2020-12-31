9662a541479cbf7c7701f4f0eea9b448138d6f85
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-31 11:16:35 -0600

    Starting outline for About page.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
-// import About from "./components/About";
+import About from "./components/About";
 // import Projects from "./components/Projects";
 // import Contact from "./components/Contact";
 // import Footer from "./components/Footer";
@@ -13,8 +13,8 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <Navbar darkMode={darkMode} setDarkMode={setDarkMode}/>
-        {/* <About />
-        <Projects />
+        <About />
+        {/* <Projects />
         <Contact />
         <Footer /> */}
         <p>Application is rendering.</p>
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../stylesheets/About.css";
+
+const About = () => {
+    return(
+        <section className="about">
+            <h2>Hello!</h2>
+            <h3>My name is Morgan, I'm a Full-Stack Web Developer based in Arkansas.</h3>
+            <p>Click here to get in touch:</p>
+            <button>Contact Me!</button>
+        </section>
+    )
+}
+
+export default About;
