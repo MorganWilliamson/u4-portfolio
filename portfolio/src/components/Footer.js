eb0c5ddbb17b746f7ef0d39854a3a6838a2de7a1
MorganWilliamson <mwilliamson7695@gmail.com>
2021-01-01 16:20:54 -0600

    Rough style in place for footer, needs work.

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -5,12 +5,12 @@ const Footer = () => {
     return (
         <section className="footer">
             <h4>Morgan Williamson</h4>
-            <a href="https://github.com/MorganWilliamson/u4-portfolio" rel="noreferrer" target="_blank" >GitHub Repo</a>
+            <a className="repo" href="https://github.com/MorganWilliamson/u4-portfolio" rel="noreferrer" target="_blank" >GitHub Repo</a>
 
             <div className="footerSM">
-                <a href="https://github.com/MorganWilliamson" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/github.svg" alt="github logo" /></a>
-                <a href="https://www.linkedin.com/in/morgan-t-williamson/" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/linkedin.svg" alt="linkedin logo"/></a>
-                <a href="https://twitter.com/MorganW_dev" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/twitter.svg" alt="twitter logo" /></a>
+                <a href="https://github.com/MorganWilliamson" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/github.svg" alt="github logo" /><p>GitHub</p></a>
+                <a href="https://www.linkedin.com/in/morgan-t-williamson/" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/linkedin.svg" alt="linkedin logo"/><p>LinkedIn</p></a>
+                <a href="https://twitter.com/MorganW_dev" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/twitter.svg" alt="twitter logo" /><p>Twitter</p></a>
             </div>
         </section>
     )
