73526b80f202c6528c8774062d86c8b4fcac53f9
MorganWilliamson <mwilliamson7695@gmail.com>
2021-01-01 16:22:23 -0600

    Removed social media text.

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -8,9 +8,9 @@ const Footer = () => {
             <a className="repo" href="https://github.com/MorganWilliamson/u4-portfolio" rel="noreferrer" target="_blank" >GitHub Repo</a>
 
             <div className="footerSM">
-                <a href="https://github.com/MorganWilliamson" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/github.svg" alt="github logo" /><p>GitHub</p></a>
-                <a href="https://www.linkedin.com/in/morgan-t-williamson/" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/linkedin.svg" alt="linkedin logo"/><p>LinkedIn</p></a>
-                <a href="https://twitter.com/MorganW_dev" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/twitter.svg" alt="twitter logo" /><p>Twitter</p></a>
+                <a href="https://github.com/MorganWilliamson" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/github.svg" alt="github logo" /></a>
+                <a href="https://www.linkedin.com/in/morgan-t-williamson/" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/linkedin.svg" alt="linkedin logo"/></a>
+                <a href="https://twitter.com/MorganW_dev" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/twitter.svg" alt="twitter logo" /></a>
             </div>
         </section>
     )
