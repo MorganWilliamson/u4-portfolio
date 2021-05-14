0bc4c6f71fd8c17865ecf61128cd1c91e32b7a0e
Morgan Williamson <morgantaylorwilliamson@protonmail.com>
2021-05-14 14:25:06 -0500

    Adjusting about section, adding hero image.

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -5,16 +5,15 @@ import "../stylesheets/About.css";
 const About = () => {
     return(
         <section className="about">
-            <div className="aboutInfo">
-                <h2>Hello!</h2>
-                <h3>My name is Morgan, I'm a Full-Stack Web Developer based in Arkansas.</h3>
-                <p>I specialize in <strong>JavaScript, React, Node.js,</strong> and <strong>HTML/CSS.</strong></p>
-                <p>I'm <strong>currently looking for work</strong> - local, remote, or otherwise!</p>
-                <p>Please click below if you'd like to get in touch:</p>
-                <Link className="aboutLink" to="/contact">Contact Me</Link>
-            </div>
             <div className="aboutImage">
-                <img src="https://images.unsplash.com/photo-1523800503107-5bc3ba2a6f81?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="grey laptop in a window at night, next to a dim lightbulb" />
+                <div className="aboutInfo">
+                    <h2>Hello!</h2>
+                    <h3>My name is Morgan, I'm a Full-Stack Web Developer based in Arkansas.</h3>
+                    <p>I specialize in <strong>JavaScript, React, Node.js,</strong> and <strong>HTML/CSS.</strong></p>
+                    <p>I'm <strong>currently looking for work</strong> - local, remote, or otherwise!</p>
+                    <p>Please click below if you'd like to get in touch:</p>
+                    <Link className="aboutLink" to="/contact">Contact Me</Link>
+                </div>
             </div>
         </section>
     )
