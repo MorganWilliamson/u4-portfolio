e82e66936117c367ab1affdb5f1aac26c1410410
Morgan Williamson <morgantaylorwilliamson@protonmail.com>
2021-05-17 15:23:00 -0500

    Initial move and positioning of social media links.

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,87 +1,20 @@
 import React from "react";
-import axios from "axios";
 import "../stylesheets/Contact.css";
 
-class Contact extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            message: ''
-        }
-    }
+const Contact = () => {
+    return(
+        <section className="contact">
+            <h2>Contact</h2>
 
-    handleNameChange = (e) => {
-        this.setState({name: e.target.value})
-    }
+            <p>You can find me at any of the sites below, please feel free to reach out anytime!</p>
 
-    handleEmailChange = (e) => {
-        this.setState({email: e.target.value})
-    }
-
-    handleMessageChange = (e) => {
-        this.setState({message: e.target.value})
-    }
-
-    resetForm = () => {
-        this.setState({ name: '', email: '', message: '' })
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault()
-        // Come back and update URL
-        axios.post('http://localhost:3000/send')
-            .then((res) => {
-                if (res.data.status === 'success') {
-                    alert("Message sent. Thank you!");
-                    this.resetForm()
-                } else if (res.data.status === 'fail') {
-                    alert("Message could not be sent.");
-                    console.log("Failure.")
-                }
-            })
-            .catch((err) => {
-                alert("The system encountered an error.")
-                console.log(err.message)
-            })
-    }
-
-    render() {
-        return (
-            <section className="contact">
-                <h2>Contact</h2>
-                
-                <form className="contactForm" onSubmit={this.handleSubmit}>
-                    <label htmlFor="name">Name:</label>
-                    <input 
-                        type="text"
-                        className="contactFormInput"
-                        value={this.state.value}
-                        onChange={this.handleNameChange}
-                    />
-
-                    <label htmlFor="email">Email:</label>
-                    <input 
-                        type="email"
-                        className="contactFormInput"
-                        value={this.state.value}
-                        onChange={this.handleEmailChange}
-                    />
-
-                    <label htmlFor="message">Message:</label>
-                    <textarea 
-                        rows="5"
-                        className="contactFormInput"
-                        value={this.state.value}
-                        onChange={this.handleMessageChange}
-                    />
-
-                    <button type="submit">Submit</button>
-                </form> 
-            </section>
-        )
-    }
-}
+            <div className="SMlinks">
+                <a href="https://github.com/MorganWilliamson" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/github.svg" alt="github logo" />GitHub</a>
+                <a href="https://www.linkedin.com/in/morgan-t-williamson/" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/linkedin.svg" alt="linkedin logo"/>LinkedIn</a>
+                <a href="https://twitter.com/MorganW_dev" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/twitter.svg" alt="twitter logo" />Twitter</a>
+            </div>
+        </section>
+    );
+};
 
 export default Contact;
--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -6,12 +6,6 @@ const Footer = () => {
         <section className="footer">
             <h4>Morgan Williamson | 2020</h4>
             <a className="repo" href="https://github.com/MorganWilliamson/u4-portfolio" rel="noreferrer" target="_blank" >GitHub Repo</a>
-
-            <div className="footerSM">
-                <a href="https://github.com/MorganWilliamson" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/github.svg" alt="github logo" /></a>
-                <a href="https://www.linkedin.com/in/morgan-t-williamson/" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/linkedin.svg" alt="linkedin logo"/></a>
-                <a href="https://twitter.com/MorganW_dev" rel="noreferrer" target="_blank"><img src="https://simpleicons.org/icons/twitter.svg" alt="twitter logo" /></a>
-            </div>
         </section>
     )
 }
